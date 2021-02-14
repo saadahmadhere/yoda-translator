9f290a95b347c544bdf488729f379cbee00890de
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-02-14 23:05:08 +0530

    done code

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-var txt = document.querySelector("#text-input");
+let txtInput= document.querySelector("#text-input");
+let button = document.querySelector(".btn");
 
-document.querySelector(".btn").addEventListener('click',function(){
+let outputDiv = document.querySelector(".output-class");
 
-    document.querySelector(".output-class").innerHTML = "fdjfd "+txt.value
+function getURL(text){
+    return "https://api.funtranslations.com/translate/yoda.json"+"?text="+text;
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("You've reached maximum request limit")
+}
+
+document.querySelector(".btn").addEventListener("click",function(){
+
+    let text = txtInput.value //taking input from text area
+
+    fetch(getURL(text))
+    .then(response=>response.json())
+    .then(json=>outputDiv.innerHTML = (json.contents.translated))
+    .catch(errorHandler)
+    
+
+
 })
-
 
